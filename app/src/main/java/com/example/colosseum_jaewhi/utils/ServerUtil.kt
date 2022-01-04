7c787757aa76cb42e6ef96c70a49961dc8b241b8
Tycoon19 <kevin960831@naver.com>
2022-01-04 15:51:07 +0900

    로그인 화면 기본 구조 구현

--- app/src/main/java/com/example/colosseum_jaewhi/utils/ServerUtil.kt
+++ app/src/main/java/com/example/colosseum_jaewhi/utils/ServerUtil.kt
@@ -4,4 +4,18 @@ class ServerUtil {
 
 //   어떤 내용? => 서버 연결 전담.
 
+//  자바의 Static과 비슷한 개념으로서 다른 곳에서 ServerUtil.BASE_URL 을 바로 사용하기 위해 companion object를 사용함.
+    companion object{
+
+//        모든 기능의 기본이 되는 주소. 많이 써야 해서 변수로 지정해버렸다잉~
+        val BASE_URL = "http://54.180.52.26"
+
+//    로그인 하는 기능 -> 함수를 만들어라
+        fun postRequestLogin(){
+
+        }
+
+
+    }
+
 }
