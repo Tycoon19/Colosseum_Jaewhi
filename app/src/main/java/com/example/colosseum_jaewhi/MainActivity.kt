2c0bd60debfb9ab15bce45d400502dc8259125a9
Tycoon19 <kevin960831@naver.com>
2022-01-05 13:27:16 +0900

    화면에서 서버 응답을 분석 / 반영할 준비 작업 완료

--- app/src/main/java/com/example/colosseum_jaewhi/MainActivity.kt
+++ app/src/main/java/com/example/colosseum_jaewhi/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.colosseum_jaewhi.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
@@ -22,8 +23,18 @@ class MainActivity : BaseActivity() {
             val inputPw = passwordEdt.text.toString()
 
 //          서버에 실제 회원이 맞는지 확인 요청 (Request)
-//          밑에다 줄줄이 적어도 되는데 너무 길어지기 때문에 코드 정리용 ServerUtil.kt에 작성하고 불러오
-            ServerUtil.postRequestLogin(inputEmail,inputPw)
+//          밑에다 줄줄이 적어도 되는데 너무 길어지기 때문에 코드 정리용 ServerUtil.kt에 작성하고 불러오기
+//          서버에 로그인하러 email, pw 갔다 와주세요. 하지만 갔다와서 뭐할건가요? ==> 일종의 가이드북!!!! 메타 가져오자ㅋㅋ
+            ServerUtil.postRequestLogin(inputEmail,inputPw,object : ServerUtil.JsonResponseHandler{
+                override fun onResponse(jsonObj: JSONObject) {
+
+//                    jsonObj : 서버에서 내려준 본문을 JSON형태로 가공해준 결과물.
+//                    => 이 내부를 파싱(분석)해서, 상황에 따른 대응.
+//                    => ex. 로그인 실패시, 그 이유를 토스트로 띄운다던지.
+
+                }
+
+            })
         }
 
     }
