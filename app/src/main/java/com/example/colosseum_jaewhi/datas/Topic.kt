9297cc6c571d1dc61528527b9d8c2e6a7d78ecd4
Tycoon19 <kevin960831@naver.com>
2022-01-09 22:11:44 +0900

    토론 주제 목록 표현용 데이터 클래스 + xml 작성

---
+++ app/src/main/java/com/example/colosseum_jaewhi/datas/Topic.kt
@@ -0,0 +1,9 @@
+package com.example.colosseum_jaewhi.datas
+
+class Topic {
+
+    var id = 0 // 나중에 Int가 들어올것이라는 명시.
+    var title = "" // 나중에 String이 들어올것이라는 명시.
+    var imageUrl = "" // 나중에 String이 들어올것이라는 명시.
+
+}
