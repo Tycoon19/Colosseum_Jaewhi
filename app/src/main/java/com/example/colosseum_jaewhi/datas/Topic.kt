3bf84f24cacb6b665f57a8b96a1678a5a2be1b07
Tycoon19 <kevin960831@naver.com>
2022-01-09 23:15:23 +0900

    JSONObject를 한 덩어리로 재료로 넣어주면 => Topic으로 변환해주는 함수 추가.

--- app/src/main/java/com/example/colosseum_jaewhi/datas/Topic.kt
+++ app/src/main/java/com/example/colosseum_jaewhi/datas/Topic.kt
@@ -1,9 +1,30 @@
 package com.example.colosseum_jaewhi.datas
 
+import org.json.JSONObject
+
 class Topic {
 
     var id = 0 // 나중에 Int가 들어올것이라는 명시.
     var title = "" // 나중에 String이 들어올것이라는 명시.
     var imageUrl = "" // 나중에 String이 들어올것이라는 명시.
+
+    companion object{
+
+//        적당한 JSONObject 하나를 넣어주면 => Topic 형태로 변환해주는 함수 작성하자. => 필요할때마다 함수를 호출하면 파싱이 알아서 되게!
+
+//        JSONObject 줄께. Topic 다오ㅎ
+        fun getTopicDataFromJson(jsonObj : JSONObject) : Topic{
+
+            val resultTopic = Topic()
+
+            resultTopic.id = jsonObj.getInt("id")
+            resultTopic.title = jsonObj.getString("title")
+            resultTopic.imageUrl = jsonObj.getString("img_url")
+
+            return resultTopic
+
+        }
+
+    }
 
 }
