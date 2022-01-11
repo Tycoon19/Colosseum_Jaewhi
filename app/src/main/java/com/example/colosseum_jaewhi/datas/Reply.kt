551ccc0135519508daa9cb49d2c16fca481cf40c
Tycoon19 <kevin960831@naver.com>
2022-01-11 23:32:16 +0900

    의견 보여주는 리스트뷰 재료 생성

---
+++ app/src/main/java/com/example/colosseum_jaewhi/datas/Reply.kt
@@ -0,0 +1,25 @@
+package com.example.colosseum_jaewhi.datas
+
+import org.json.JSONObject
+
+class Reply {
+
+    var id = 0
+    var content = ""
+
+    companion object{
+
+        fun getReplyFromJson(jsonObj : JSONObject) : Reply{
+
+            val resultReply = Reply()
+
+            resultReply.id = jsonObj.getInt("id")
+            resultReply.content = jsonObj.getString("content")
+
+            return resultReply
+
+        }
+
+    }
+
+}
