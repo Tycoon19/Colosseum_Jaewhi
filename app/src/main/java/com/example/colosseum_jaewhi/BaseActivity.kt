5056eecc473abb5c055a6a301382879fb914491c
Tycoon19 <kevin960831@naver.com>
2022-01-12 16:45:36 +0900

    상속 이용 => 뒤로가기 기능 일괄 부여

--- app/src/main/java/com/example/colosseum_jaewhi/BaseActivity.kt
+++ app/src/main/java/com/example/colosseum_jaewhi/BaseActivity.kt
@@ -1,11 +1,15 @@
 package com.example.colosseum_jaewhi
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
 abstract class BaseActivity : AppCompatActivity() {
+
+    // 밑에서 자동완성해서 쓰지말고 여기서 lateinit var 로 선언해주자.
+    lateinit var backBtn : ImageView
 
     val mContext = this
 
@@ -27,6 +31,11 @@ abstract class BaseActivity : AppCompatActivity() {
 
         val myToolbar = defaultActionBar.customView.parent as Toolbar
         myToolbar.setContentInsetsAbsolute(0,0)
+
+        backBtn = defaultActionBar.customView.findViewById(R.id.backBtn)
+        backBtn.setOnClickListener {
+            finish()
+        }
 
     }
 
