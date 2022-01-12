9389ef58d17dbaf22b42cda1ae56ff6a749547df
Tycoon19 <kevin960831@naver.com>
2022-01-12 13:33:14 +0900

    의견 파싱할때 => 선택 진영이 어딘지고 같이 파싱

--- app/src/main/java/com/example/colosseum_jaewhi/datas/Reply.kt
+++ app/src/main/java/com/example/colosseum_jaewhi/datas/Reply.kt
@@ -7,6 +7,8 @@ class Reply {
     var id = 0
     var content = ""
 
+    lateinit var selectedSide : Side
+
     companion object{
 
         fun getReplyFromJson(jsonObj : JSONObject) : Reply{
@@ -15,6 +17,9 @@ class Reply {
 
             resultReply.id = jsonObj.getInt("id")
             resultReply.content = jsonObj.getString("content")
+
+//            중괄호 따서 대입해서 한 큐에 넣어주기.
+            resultReply.selectedSide = Side.getSideFromJson(jsonObj.getJSONObject("selected_side"))
 
             return resultReply
 
